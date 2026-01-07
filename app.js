dfe5a442904c8e0dae5492fe26bb00de5a8d6cd0
nelutulezeu <nelutu.lezeu@gmail.com>
2026-01-08 01:17:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -142,23 +142,22 @@ function attachSpecIcon(selectId) {
   const icon = card.querySelector('.spec-icon');
 
   const updateIcon = () => {
-    const specUrl = select.dataset.specUrl;
+    const opt = select.selectedOptions[0];
 
-    if (specUrl) {
+    if (opt && opt.dataset.specUrl) {
       icon.hidden = false;
       icon.onclick = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        window.open(specUrl, '_blank', 'noopener');
+        window.open(opt.dataset.specUrl, '_blank', 'noopener');
       };
     } else {
       icon.hidden = true;
-      icon.onclick = null;
     }
   };
 
   select.addEventListener('change', updateIcon);
-  updateIcon();
+  updateIcon(); 
 }
 
 
