a765db93c4e5fec1425c086f32b45f2b38c88efe
nelutulezeu <nelutu.lezeu@gmail.com>
2026-01-10 15:18:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -267,6 +267,7 @@ function handleSubmit() {
         data.fieldName !== 'Informations'
       ).map(data => ({ field: data.fieldName, value: data.fieldValue })),
     };
+    console.log(templateParams);
   }
 }
 
